c2046b70cc582db350e32d50e6a4f0b3bced744c
Steffanie Maher <38441171+ScarHolmes@users.noreply.github.com>
2018-10-18 14:55:33 -0500

    Update duplicate.js
    So it only worked if it was duplicated once had to find a solution if having multiple of one number

--- duplicate.js
+++ duplicate.js
@@ -1,17 +1,35 @@
-var test = [1,1,3,3,6,6,6,7,8]
+var test = [1,1,3,6,6,6,7,7,7,7,8,9,9,9,9]
 function dup_array(nums){
-     	var count = 0;
-     	for(var i = 0; i< nums.length-1; i++) {
-        if(nums[i] != nums[i+1]){
-            count ++;
-            nums[count] = nums[i+1];
-        }
+  var count = 0;
+  for(var i = 0; i<= nums.length-1-count; i++) {
+    if(nums[i] === nums[i+1]){
+      count ++;
+      console.log("This is index ", i);
+      console.log(nums);
+      for(var p=i+1; p<nums.length-1; p++){
+        nums[p] = nums[p+1];
+      }
+      // nums[count] = nums[i+1];
+      // console.log("this is nums " + nums[count]);
+      }
+      if(nums[i] === nums[i+1]){
+       i--;
+     }
+  }
+     
+
+    
+         console.log(count, "This is index jjjjjjjjj ", i);
+      console.log(nums);
+  
+    
+  for(var x=count-1; x>0; x--){
+    nums.pop();
     }
-    for(var x=count; x>0; x--){
-        nums.pop();
-    }
-    return count;
+  console.log(count)
+  return count;
 }
+
 
             
 dup_array(test)
